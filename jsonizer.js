2bc1d4db65a7818021bae3a63c5d3fee853070cc
Luis Pablo Gallo <luispablo.gallo@gmail.com>
2021-05-11 15:40:36 -0300

    Apply sanity-check when adding rows
    After adding the new row, review the entire content, and if any FK relation tuple is missing, fetch it from the DB and add it.

--- jsonizer.js
+++ jsonizer.js
@@ -1,5 +1,49 @@
 #!/usr/bin/env node
 
+const addRow = async function addRow (fixtures, knex, table, id) {
+  const [row] = await knex(table).where({ id });
+  if (!row) throw new Error(`${table}#${id} not found!`);
+  if(!fixtures[table]) fixtures[table] = [];
+  fixtures[table].push(row);
+};
+
+const getTableFKs = async function getTableFKs (knex, table) {
+  const res = await knex.raw(`
+    SELECT  tc.table_name
+    ,       kcu.column_name
+    ,       ccu.table_name    AS foreign_table_name
+    ,       ccu.column_name   AS foreign_column_name 
+    FROM    information_schema.table_constraints AS tc 
+    JOIN    information_schema.key_column_usage AS kcu 
+    ON      tc.constraint_name = kcu.constraint_name 
+    AND     tc.table_schema = kcu.table_schema
+    JOIN    information_schema.constraint_column_usage AS ccu 
+    ON      ccu.constraint_name = tc.constraint_name 
+    AND     ccu.table_schema = tc.table_schema
+    WHERE   tc.constraint_type = 'FOREIGN KEY' 
+    AND     tc.table_name = '${table}';
+  `);
+  return res.rows;
+};
+
+const sanityCheck = async function sanityCheck (fixtures, knex) {
+  for (table of Object.keys(fixtures)) {
+    const tableFks = await getTableFKs(knex, table);
+
+    for (row of fixtures[table]) {
+      // Review each row non-empty FKs values
+      const presentFKs = tableFks.filter(fk => row[fk.column_name]);
+
+      for (fk of presentFKs) {
+        const rowFKValue = row[fk.column_name];
+        const fixtureTable = fixtures[fk.foreign_table_name]; 
+        const [fkRow] = (fixtureTable || []).filter(r => r[fk.foreign_column_name] === rowFKValue);
+        if (!fixtureTable || !fkRow) await addRow(fixtures, knex, fk.foreign_table_name, rowFKValue);
+      }
+    }
+  }
+};
+
 (async function main () {
   try {
     const FILENAME = process.env.FILENAME || "fixtures.json";
@@ -7,17 +51,23 @@
     const target = process.argv[2].split("#");
     const [targetTable, targetID] = target;
 
-    const fs = require("fs");  
+    const fs = require("fs");
     const fixtures = fs.existsSync(FILENAME) ? JSON.parse(fs.readFileSync(FILENAME)) : {};
     const knex = require("knex")({ client: "pg", connection: { database }});
-    const [row] = await knex(targetTable).where({ id: targetID });
+    const [currentValue] = (fixtures[targetTable] || []).filter(r => r.id === parseInt(targetID));
 
-    if(!fixtures[targetTable]) fixtures[targetTable] = [];
-    fixtures[targetTable].push(row);
-    
-    fs.writeFileSync(FILENAME, JSON.stringify(fixtures, null, 2));
-
-    process.exit(0);
+    if (currentValue) {
+      console.error(`${targetTable} with ID ${targetID} already exists in JSON file`);
+      process.exit(1);
+    } else {
+      // Add requested row
+      await addRow(fixtures, knex, targetTable, targetID);
+      // Fix any issue
+      await sanityCheck(fixtures, knex);
+      // Finally write to file
+      fs.writeFileSync(FILENAME, JSON.stringify(fixtures, null, 2));
+      process.exit(0);
+    }
   } catch (err) {
     console.error(err);
     process.exit(1);
